6707cdc340823d05cbb54240dbd3329e7fdea799
s2iwi2s <s2iwi2s@gmail.com>
2023-08-03 15:14:32 -0500

    add home, navbar and footer templates

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {navbarRoute} from "./layouts/navbar/navbar.route";
 
-const routes: Routes = [];
+const LAYOUT_ROUTES = [navbarRoute];
+const routes: Routes = [...LAYOUT_ROUTES];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeModule} from "./home/home.module";
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    HomeModule,
     AppRoutingModule
   ],
   providers: [],
