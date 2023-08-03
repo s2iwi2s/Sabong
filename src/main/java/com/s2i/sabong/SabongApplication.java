ea961a1e81bea367f82ca23f002c4c8489a86b74
s2iwi2s <s2iwi2s@gmail.com>
2023-08-03 08:10:59 -0500

    add

---
+++ src/main/java/com/s2i/sabong/SabongApplication.java
@@ -0,0 +1,13 @@
+package com.s2i.sabong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s2i/sabong/SabongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2i.sabong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
